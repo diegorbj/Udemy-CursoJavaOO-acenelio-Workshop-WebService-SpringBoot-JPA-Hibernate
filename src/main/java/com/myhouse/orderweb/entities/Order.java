50cdc4c9d5ab5b3eefcd75ad0b7171d93a4fd1db
Diego Barros <diegorbj@outlook.com>
2021-03-24 19:13:47 -0300

    OrderStatus anum

--- src/main/java/com/myhouse/orderweb/entities/Order.java
+++ src/main/java/com/myhouse/orderweb/entities/Order.java
@@ -1,6 +1,7 @@
 package com.myhouse.orderweb.entities;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
+import com.myhouse.orderweb.entities.enums.OrderStatus;
 import lombok.*;
 
 import javax.persistence.*;
@@ -8,8 +9,6 @@ import java.io.Serializable;
 import java.time.Instant;
 
 @NoArgsConstructor
-@Getter
-@Setter
 @ToString
 @Entity
 @Table(name = "tb_order")
@@ -20,18 +19,37 @@ public class Order implements Serializable {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @EqualsAndHashCode.Include
+    @Getter
+    @Setter
     private Long id;
 
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss'Z'", timezone = "GMT")
+    @Getter
+    @Setter
     private Instant moment;
+
+    private Integer orderStatus;
 
     @ManyToOne
     @JoinColumn(name = "client_id")
+    @Getter
+    @Setter
     private User client;
 
-    public Order(Long id, Instant moment, User client) {
+    public Order(Long id, Instant moment, OrderStatus orderStatus, User client) {
         this.id = id;
         this.moment = moment;
+        setOrderStatus(orderStatus);
         this.client = client;
+    }
+
+    public OrderStatus getOrderStatus() {
+        return OrderStatus.valueOf(orderStatus);
+    }
+
+    public void setOrderStatus(OrderStatus orderStatus) {
+        if (orderStatus != null) {
+            this.orderStatus = orderStatus.getCode();
+        }
     }
 }
