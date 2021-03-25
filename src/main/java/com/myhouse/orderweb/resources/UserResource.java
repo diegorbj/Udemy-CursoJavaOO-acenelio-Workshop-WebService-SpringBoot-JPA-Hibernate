a28238f76eb80f59919d7e210400fe80c920b82e
Diego Barros <diegorbj@outlook.com>
2021-03-25 09:30:13 -0300

    User insert

--- src/main/java/com/myhouse/orderweb/resources/UserResource.java
+++ src/main/java/com/myhouse/orderweb/resources/UserResource.java
@@ -4,11 +4,11 @@ import com.myhouse.orderweb.entities.User;
 import com.myhouse.orderweb.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.lang.module.ResolutionException;
+import java.net.URI;
 import java.util.List;
 
 @RestController
@@ -18,15 +18,29 @@ public class UserResource {
     @Autowired
     private UserService _service;
 
-    @RequestMapping(method = RequestMethod.GET)
+    //@RequestMapping(method = RequestMethod.GET)
+    @GetMapping
     public ResponseEntity<List<User>> findAll() {
         List<User> list = _service.findAll();
         return ResponseEntity.ok().body(list);
     }
 
-    @RequestMapping(value = "/{id}", method = RequestMethod.GET)
+    //@RequestMapping(value = "/{id}", method = RequestMethod.GET)
+    @GetMapping(value = "/{id}")
     public ResponseEntity<User> findById(@PathVariable Long id) {
         User obj = _service.findById(id);
         return ResponseEntity.ok().body(obj);
+    }
+
+    @PostMapping
+    public ResponseEntity<User> insert(@RequestBody User obj) {
+        obj = _service.insert(obj);
+        //return ResponseEntity.ok().body(obj);
+        URI uri = ServletUriComponentsBuilder
+                .fromCurrentRequest()
+                .path("/{id}")
+                .buildAndExpand(obj.getId())
+                .toUri();
+        return ResponseEntity.created(uri).body(obj);
     }
 }
