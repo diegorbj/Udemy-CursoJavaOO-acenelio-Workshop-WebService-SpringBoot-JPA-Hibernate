dbc65e22e63e8251408cbd48e46ef7d1feb1a81e
Diego Barros <diegorbj@outlook.com>
2021-03-24 22:27:58 -0300

    OrderItem, many-to-many association with extra attributes PART 1

---
+++ src/main/java/com/myhouse/orderweb/entities/OrderItem.java
@@ -0,0 +1,53 @@
+package com.myhouse.orderweb.entities;
+
+import com.myhouse.orderweb.entities.pk.OrderItemPK;
+import lombok.*;
+
+import javax.persistence.Embedded;
+import javax.persistence.EmbeddedId;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+import java.io.Serializable;
+
+@NoArgsConstructor
+@ToString
+@EqualsAndHashCode
+@Entity
+@Table(name = "tb_order_item")
+public class OrderItem implements Serializable {
+
+    @EqualsAndHashCode.Include
+    @EmbeddedId
+    private OrderItemPK id;
+
+    @Getter
+    @Setter
+    private Integer quantity;
+
+    @Getter
+    @Setter
+    private Double price;
+
+    public OrderItem(Order order, Product product, Integer quantity, Double price) {
+        this.id.setOrder(order);
+        this.id.setProduct(product);
+        this.quantity = quantity;
+        this.price = price;
+    }
+
+    public Order getOrder() {
+        return this.id.getOrder();
+    }
+
+    public void setOrder(Order order) {
+        this.id.setOrder(order);
+    }
+
+    public Product getProduct() {
+        return this.id.getProduct();
+    }
+
+    public void setProduct(Product product) {
+        this.id.setProduct(product);
+    }
+}
---
+++ src/main/java/com/myhouse/orderweb/entities/pk/OrderItemPK.java
@@ -0,0 +1,27 @@
+package com.myhouse.orderweb.entities.pk;
+
+import com.myhouse.orderweb.entities.Order;
+import com.myhouse.orderweb.entities.Product;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.Embeddable;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import java.io.Serializable;
+
+@Getter
+@Setter
+@EqualsAndHashCode
+@Embeddable
+public class OrderItemPK implements Serializable {
+
+    @ManyToOne
+    @JoinColumn(name = "order_id")
+    private Order order;
+
+    @ManyToOne
+    @JoinColumn(name = "product_id")
+    private Product product;
+}
